d736b77843f36bd7a2208a6e540ac7f236699b15
masleon <masleon@mail.ru>
2022-11-19 17:45:03 +0300

    Add files via upload

---
+++ src/main/java/com/geekbrains/spring/web/dto/ProductDto.java
@@ -0,0 +1,16 @@
+package com.geekbrains.spring.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductDto {
+    private Long id;
+    private String title;
+    private Integer price;
+}
